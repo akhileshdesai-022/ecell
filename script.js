726b70c03501830d919854e1f105e3fa8f2691d1
akhilesh <akhileshdesai19@gmail.com>
2024-09-19 21:48:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('submitBtn').addEventListener('click', function(event) {
+    event.preventDefault(); // prevent form submission
+    // your custom code here
+    console.log('Submit button clicked!');
+  });
+  
+  
